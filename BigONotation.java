7bc6480ff482283268d072c530cbe8e8d2c3ec3b
Lawrence Field <lfield@bu.edu>
2017-04-19 15:48:09 -0400

    Chapter 1: Big O notation and Binary Search
    In Chapter 1 of Grokking Algorithms: I show the difference of each type of Big O notation and Binary Search.
    Goals: 
    I am refreshing myself on Java and slowly adding to my github portfolio everything I have lost or at least something equivalent to it.

---
+++ BigONotation.java
@@ -0,0 +1,213 @@
+import java.util.Arrays;
+
+/**
+ * Created by Lawrence Field on 4/19/2017.
+ */
+public class BigONotation {
+
+    private int[] theArray;
+    private int arraySize;
+    private int itemsInArray = 0;
+    static long startTime;
+    static long endTime;
+
+    public static void main(String[] args) {
+
+        BigONotation testAlgo = new BigONotation(10);
+        testAlgo.addItemToArray(10);
+        System.out.println(Arrays.toString(testAlgo.theArray));
+
+        BigONotation testAlgo2 = new BigONotation(100000);
+        testAlgo2.generateRandomArray();
+        BigONotation testAlgo3 = new BigONotation(200000);
+        testAlgo3.generateRandomArray();
+        BigONotation testAlgo4 = new BigONotation(300000);
+        testAlgo4.generateRandomArray();
+        BigONotation testAlgo5 = new BigONotation(400000);
+        testAlgo5.generateRandomArray();
+
+        testAlgo2.linearSearchForValue(20);
+        testAlgo3.linearSearchForValue(20);
+        testAlgo4.linearSearchForValue(20);
+        testAlgo5.linearSearchForValue(20);
+
+        BigONotation testAlgo6 = new BigONotation(10000);
+        testAlgo6.generateRandomArray();
+        BigONotation testAlgo7 = new BigONotation(20000);
+        testAlgo7.generateRandomArray();
+        BigONotation testAlgo8 = new BigONotation(30000);
+        testAlgo8.generateRandomArray();
+        BigONotation testAlgo9 = new BigONotation(40000);
+        testAlgo9.generateRandomArray();
+
+        testAlgo6.bubbleSort();
+        testAlgo7.bubbleSort();
+        testAlgo8.bubbleSort();
+        testAlgo9.bubbleSort();
+
+        testAlgo6.binarySearchForValue(20);
+        testAlgo7.binarySearchForValue(20);
+        testAlgo8.binarySearchForValue(20);
+        testAlgo9.binarySearchForValue(20);
+
+        startTime = System.currentTimeMillis();
+
+        testAlgo2.quickSort(0, testAlgo2.itemsInArray);
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort took " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+
+        testAlgo3.quickSort(0, testAlgo3.itemsInArray);
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort took " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+
+        testAlgo4.quickSort(0, testAlgo4.itemsInArray);
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort took " + (endTime - startTime));
+
+        startTime = System.currentTimeMillis();
+
+        testAlgo5.quickSort(0, testAlgo5.itemsInArray);
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Quick Sort took " + (endTime - startTime));
+
+    }
+    // O(1)
+    public void addItemToArray (int newItem) {
+
+        theArray [itemsInArray++] = newItem;
+    }
+    // O(N)
+    public void linearSearchForValue (int value) {
+
+        boolean valueInArray = false;
+        String indexsWithValue = "";
+
+        startTime = System.currentTimeMillis();
+
+        for (int i = 0; i <arraySize; i++){
+
+            if (theArray[i] == value){
+                valueInArray= true;
+                indexsWithValue += i + " ";
+            }
+        }
+
+        System.out.println("Value Fount: " +valueInArray);
+        endTime = System.currentTimeMillis();
+        System.out.println("Linear Search Took: " + (endTime-startTime));
+    }
+    // O(N^2)
+    public void bubbleSort(){
+
+        startTime = System.currentTimeMillis();
+
+        for (int i = arraySize -1 ; i > 1; i--) {
+
+            for (int j = 0; j<i; j++){
+
+                if (theArray[j] > theArray[j+1]) {
+                    swapValues(j , j+1);
+                }
+            }
+        }
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Bubble Sort Took: " + (endTime-startTime));
+    }
+    // O(logN)
+    public void binarySearchForValue (int value){
+
+        startTime = System.currentTimeMillis();
+
+        int lowIndex = 0;
+        int highIndex = arraySize - 1;
+
+        int timesThrough = 0;
+
+        while (lowIndex <= highIndex){
+            int middleIndex = (highIndex + lowIndex) / 2;
+
+            if (theArray[middleIndex] < value)
+                lowIndex = middleIndex + 1;
+
+                else if(theArray[middleIndex] > value)
+                    highIndex = middleIndex - 1;
+
+                else {
+                    System.out.println("Found a match in index " + middleIndex);
+                    lowIndex = highIndex + 1;
+            }
+
+            timesThrough++;
+        }
+
+        endTime = System.currentTimeMillis();
+        System.out.println("Binary Search Sort Took: " + (endTime-startTime));
+        System.out.println("Times Through : " + timesThrough);
+    }
+    // O(N*logN)
+    public void quickSort(int left, int right){
+
+        if(right - left <=0)
+            return;
+
+        else {
+            int pivot = theArray[right];
+            int pivotLoation = partitionArray(left,right,pivot);
+            quickSort(left, pivotLoation - 1);
+            quickSort(pivotLoation + 1, right);
+        }
+    }
+
+    public int partitionArray(int left,int right,int pivot) {
+        int leftPointer = left - 1;
+        int rightPointer = right;
+
+        while (true) {
+            while (theArray[++leftPointer] < pivot);
+
+            while (rightPointer > 0 && theArray[--rightPointer] > pivot);
+
+            if (leftPointer >= rightPointer){
+                break;
+            }
+            else {
+                swapValues(leftPointer, rightPointer);
+            }
+        }
+
+        swapValues(leftPointer, right);
+        return leftPointer;
+    }
+    // Constructor
+    BigONotation(int size) {
+
+        arraySize = size;
+        theArray = new int[size];
+    }
+
+    public void generateRandomArray() {
+        for (int i = 0; i < arraySize; i++) {
+
+            theArray[i] = (int) (Math.random() * 1000) + 10;
+        }
+
+        itemsInArray = arraySize - 1;
+    }
+
+    public void swapValues (int indexOne, int indexTwo) {
+
+        int temp = theArray[indexOne];
+        theArray [indexOne] = theArray[indexTwo];
+        theArray[indexTwo] = temp;
+    }
+
+}
